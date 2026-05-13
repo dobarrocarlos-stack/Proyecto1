7fcf676a2fed63b53fbc9d4833e33c8b02b07e30
Viti <victorberezo@gmail.com>
2026-05-13 20:02:42 +0200

    TB y reviewer

--- principal.py
+++ principal.py
@@ -57,24 +57,78 @@ def copyMonth():
     closeExcel(wb, app)
 
 
-def createFDD():
+def createReviewer():
 
-    app = xw.App(visible=False) 
-    wb = app.books.open(r"plantilla.xlsx") 
+    app = xw.App(visible=False)
 
-    nombre_fdd = "FDD Data J658"
+    wb_plantilla = app.books.open(r"plantilla.xlsx")
+    wb_reviewer = app.books.open(r"reviewer.xlsx")
 
-    if nombre_fdd in [s.name for s in wb.sheets]:
-        wb.sheets[nombre_fdd].delete()
+    hoja_origen = wb_reviewer.sheets[0]
 
-    # Crear nueva hoja limpia
-    nueva_fdd = wb.sheets.add(nombre_fdd, after=wb.sheets[-1])
-    closeExcel(wb, app)
+    nombre_hoja = "Reviewer"
+
+    if nombre_hoja in [s.name for s in wb_plantilla.sheets]:
+        wb_plantilla.sheets[nombre_hoja].delete()
+
+    hoja_destino = wb_plantilla.sheets.add(nombre_hoja, after=wb_plantilla.sheets[-1])
+
+    rango_usado = hoja_origen.used_range
+
+    hoja_destino.range("A1").value = rango_usado.value
+
+    rango_usado.api.Copy(hoja_destino.range("A1").api)
+
+    wb_plantilla.save()
+
+    wb_reviewer.close()
+    wb_plantilla.close()
+
+    app.quit()
+
+def createTb():
+
+    app = xw.App(visible=False)
+
+    wb_plantilla = app.books.open(r"plantilla.xlsx")
+    wb_reviewer = app.books.open(r"tb.xlsx")
+
+    hoja_origen = wb_reviewer.sheets[0]
+
+    nombre_hoja = "FDD Data J658"
+
+    if nombre_hoja in [s.name for s in wb_plantilla.sheets]:
+        wb_plantilla.sheets[nombre_hoja].delete()
+
+    hoja_destino = wb_plantilla.sheets.add(nombre_hoja, after=wb_plantilla.sheets[-1])
+
+    rango_usado = hoja_origen.used_range
+
+    # Copiar datos
+    hoja_destino.range("A1").value = rango_usado.value
+
+    # Copiar formato
+    rango_usado.api.Copy(hoja_destino.range("A1").api)
+
+    # Última fila con datos en columna A
+    ultima_fila = hoja_destino.range("A1048576").end("up").row
+
+    # Concatenar A + C + B en columna K
+    hoja_destino.range(f"K2:K{ultima_fila}").formula = "=A2&C2&B2"
+
+    wb_plantilla.save()
+
+    wb_reviewer.close()
+    wb_plantilla.close()
+
+    app.quit()
+
 
 def menu():
     print("MENU")
     print("1) create BSC Data")
-    print("2) create FDD")
+    print("2) create tb")
+    print("3) crear Reviewer")
 
     option = int(input("Elige una opcion: "))
 
@@ -85,8 +139,10 @@ option = menu()
 
 if option == 1:
     copyMonth()
-elif option ==2:
-    createFDD()    
+elif option == 2:
+    createTb()
+elif option == 3:
+    createReviewer()
 
 
 # columna A hasta la FD, FE cancotenar r2,g2,ae22
